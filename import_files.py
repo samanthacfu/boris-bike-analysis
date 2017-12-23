155928a00d48bac74da646d9f6836f1b344b6fd8
Samantha Fu <samanthacfu@gmail.com>
2017-12-23 09:24:16 +0100

    fix formatting

--- import_files.py
+++ import_files.py
@@ -1,13 +1,17 @@
 #Import csvs and combine 
 
+#Imports
 import glob
 import os
 import pandas as pd
 import numpy as np
+import pickle
 
+#Get all csvs
 path = '/Users/samanthafu/Boris Bike Data/'        
 files = glob.glob(os.path.join(path, "*.csv"))     
 
+#Import and append
 df = pd.DataFrame()
 
 for file in files:
@@ -26,6 +30,5 @@ df["End Date"] = pd.to_datetime(df["End Date"],infer_datetime_format=True)
 df["Start Date"] = pd.to_datetime(df["Start Date"],infer_datetime_format=True)
 
 #Save down dataset
-import pickle
 with open('boris_bike_data.pickle', 'wb') as output:
     pickle.dump(df, output)
