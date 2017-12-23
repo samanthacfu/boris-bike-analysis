72d5c668cc5eabcbabc0d18ef4193811013d209e
Samantha Fu <samanthacfu@gmail.com>
2017-12-23 09:34:43 +0100

    clean comments

--- create_static_maps.py
+++ create_static_maps.py
@@ -1,83 +1,43 @@
 #Make static maps!
 
+#Imports
 import pandas as pd
+from bokeh.io import output_file, output_notebook, show
+from bokeh.models import (
+  GMapPlot, GMapOptions, ColumnDataSource, Circle, DataRange1d, PanTool, WheelZoomTool, BoxSelectTool, HoverTool
+)
+from bokeh.models.mappers import CategoricalColorMapper
+from bokeh.palettes import brewer
+from bokeh.io import curdoc
+from bokeh.layouts import column, row, widgetbox
 
+#Gmaps in Bokeh requires a Google API key from https://developers.google.com/maps/documentation/javascript/get-api-key
+API_KEY = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
+
+#Import station data
 file = '/Users/samanthafu/stations_clean.csv' 
 stations = pd.read_csv(file, skiprows=0)
 
-#MAP BY BOROUGH
-from bokeh.io import output_file, output_notebook, show
-from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
-    DataRange1d, PanTool, WheelZoomTool, BoxSelectTool, HoverTool, LabelSet
-)
-from bokeh.models.mappers import ColorMapper, LinearColorMapper, CategoricalColorMapper
-from bokeh.palettes import brewer
+#Create static map by year added
 
-map_options = GMapOptions(lat=51.504817, lng=-0.100186, map_type="roadmap", zoom=13)
-
-plot = GMapPlot(
-    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, plot_width=1300, plot_height=800
-)
-plot.title.text = "Stations by Borough"
-plot.title.text_font = "amatic sc"
-plot.title.align = "center"
-plot.title.text_font_size="40px"
-
-plot.api_key = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
-
-source = ColumnDataSource(
-    data=dict(
-        lat=stations.lat.tolist(),
-        lon=stations.lon.tolist(),
-        size=stations.docks.tolist(),
-        color=stations.borough.tolist(), 
-        name=stations.station.tolist()
-    )
-)
-
-
-#labels = LabelSet(x='lon', y='lat', text='station', level='glyph', x_offset=5, y_offset=5, source=source, render_mode='canvas')
-
-color_mapper = CategoricalColorMapper(factors=['City of London','Islington','Camden','Westminster','Kensington and Chelsea','Hackney','Wandsworth','Lambeth','Southwark','Tower Hamlets','Hammersmith and Fulham'], palette=brewer['Set3'][11])
-circle = Circle(x="lon", y="lat", size="size", fill_color=dict(field='color',transform=color_mapper), fill_alpha=0.8, line_color=None)
-plot.add_glyph(source, circle)
-
-hover= HoverTool(tooltips = [
-    ("station","@name"),
-    ("docks","@size")
-])
-
-#plot.add_layout(labels)
-plot.add_tools(PanTool(), WheelZoomTool(), hover)
-
-#output_file("stations_by_borough.html")
-#output_notebook()
-show(plot)
-
-#MAP BY YEAR ADDED
-from bokeh.io import output_file, output_notebook, show
-from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
-    DataRange1d, PanTool, WheelZoomTool, BoxSelectTool, HoverTool, Text
-)
-from bokeh.models.mappers import ColorMapper, LinearColorMapper, CategoricalColorMapper
-from bokeh.palettes import brewer, Viridis
-from bokeh.layouts import column, row, widgetbox
-
+#Set center of map to central London
 map_options = GMapOptions(lat=51.504817, lng=-0.100186, map_type="roadmap", zoom=13)
 
 plot = GMapPlot(
     x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, plot_width=1300, plot_height=700
 )
 
+#Format title
 plot.title.text = "Stations by Year Added"
 plot.title.text_font = "amatic sc"
 plot.title.align = "center"
 plot.title.offset = -20
 plot.title.text_font_size="40px"
 
-plot.api_key = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
+#Set api key
+plot.api_key = API_KEY
+
+#Create column data source
 
 source = ColumnDataSource(
     data=dict(
@@ -93,6 +53,7 @@ color_mapper = CategoricalColorMapper(factors=[2017,2016,2015,2014,2013,2012,201
 circle = Circle(x="lon", y="lat",size="size",fill_color=dict(field='color',transform=color_mapper), fill_alpha=0.8, line_color=None)
 plot.add_glyph(source, circle)
 
+#Add tools
 hover= HoverTool(tooltips = [
     ("station","@name"),
     ("docks","@size"),
