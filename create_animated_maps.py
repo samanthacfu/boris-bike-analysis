a10a17a3dbb05e65e898debadfb45ad14f14e51c
Samantha Fu <samanthacfu@gmail.com>
2017-12-23 09:12:40 +0100

    fix button and formatting:

--- create_animated_maps.py
+++ create_animated_maps.py
@@ -1,42 +1,55 @@
 #Create animated maps!!
-	#Resources
-	#https://rebeccabilbro.github.io/interactive-viz-bokeh/
-	#http://www.bigendiandata.com/2017-06-27-Mapping_in_Jupyter/
-	#https://automating-gis-processes.github.io/2016/Lesson5-interactive-map-bokeh.html
-	#https://data-dive.com/cologne-bike-rentals-interactive-map-bokeh-dynamic-choropleth 
+#resources
+#https://rebeccabilbro.github.io/interactive-viz-bokeh/
+#http://www.bigendiandata.com/2017-06-27-Mapping_in_Jupyter/
+#https://automating-gis-processes.github.io/2016/Lesson5-interactive-map-bokeh.html
+#https://data-dive.com/cologne-bike-rentals-interactive-map-bokeh-dynamic-choropleth 
 
-#Hourly map of average daily rentals and returns
-
+#Imports
 import pandas as pd
-
-file = '/Users/samanthafu/input_hourly_fixed.csv' 
-df = pd.read_csv(file, skiprows=0)
-
 from bokeh.io import output_file, output_notebook, show
 from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
-    DataRange1d, PanTool, WheelZoomTool, BoxSelectTool, CustomJS, Slider, Toggle, Range1d, HoverTool, Text
+  GMapPlot, GMapOptions, ColumnDataSource, Circle, PanTool, WheelZoomTool, CustomJS, Slider, Range1d, HoverTool, Text
 )
-from bokeh.models.mappers import ColorMapper, LinearColorMapper, CategoricalColorMapper
+from bokeh.models.mappers import CategoricalColorMapper
 from bokeh.palettes import Viridis
 from bokeh.layouts import column, row, widgetbox
+from bokeh.io import curdoc
+from bokeh.resources import INLINE
 
-map_options = GMapOptions(lat=51.504817, lng=-0.100186, map_type="roadmap", zoom=13)
+#Import hourly data
+file = '/Users/samanthafu/input_hourly_fixed.csv' 
+df_hourly = pd.read_csv(file, skiprows=0)
+
+#Import annual data
+file = '/Users/samanthafu/stations.csv' 
+df_annual = pd.read_csv(file, skiprows=0)
+
+#Gmaps in Bokeh requires a Google API key from https://developers.google.com/maps/documentation/javascript/get-api-key
+API_KEY = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
+
+#Create animated hourly map
+
+#Set center of map to central London
+map_options = GMapOptions(lat=51.504817, lng=-0.100186, map_type="roadmap", zoom=13) 
 
 plot = GMapPlot(
-    x_range=Range1d(), y_range=Range1d(), map_options=map_options, plot_width=1300, plot_height=700
+    x_range=Range1d(), y_range=Range1d(), map_options=map_options, plot_width=1300, plot_height=600
 )
-#plot.title.text = "Hourly Usage"
+
+#Format map title
 plot.title.text_font = "amatic sc"
 plot.title.align = "center"
 plot.title.text_font_size="40px"
 
-plot.api_key = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
+#Set api key
+plot.api_key = API_KEY
 
+#Create list of datasets to call
 sources = {}
 
 for x in range(0, 24):
-    newdf = df[df['hour']==x]
+    newdf = df_hourly[df_hourly['hour']==x]
     sources['_{0}'.format(x)] = ColumnDataSource(
         data=dict(
             lat=newdf.lat.tolist(),
@@ -48,7 +61,8 @@ for x in range(0, 24):
             
         )
     )
-
+    
+#Create different colored circles for rentals and returns
 circle_start = Circle(x="lon", y="lat", size="size_start", fill_color='red', fill_alpha=0.8, line_color=None)
 circle_end = Circle(x="lon", y="lat", size="size_end", fill_color='blue', fill_alpha=0.8, line_color=None)
                       
@@ -56,7 +70,6 @@ plot.add_glyph(sources['_0'], circle_start)
 plot.add_glyph(sources['_0'], circle_end)
 
 #Add tools
-
 hover= HoverTool(tooltips = [
     ("station","@name"),
     ("starting journeys","@size_start"),
@@ -66,12 +79,10 @@ hover= HoverTool(tooltips = [
 plot.add_tools(PanTool(), WheelZoomTool(),hover)
 
 #Create dict of sources
-
 dictionary_of_sources = dict(zip([x for x in range(0,24)], ['_%s' % x for x in range(0,24)]))
 js_source_array = str(dictionary_of_sources).replace("'", "")
 
 #Add slider with callback to update data source
-
 renderer_source = sources['_0']
 code = """
     var year = cb_obj.value,
@@ -85,12 +96,13 @@ slider = Slider(start=0, end=23, value=0, step=1, title="Time of Day",  callback
 
 callback.args["renderer_source"] = renderer_source
 
+#Add animation
 callbackAnime = CustomJS(args=sources, code="""
         var f = cb_obj.active;
         var j = 0;
-
+        
         if(f == true){
-            var mytimer = setInterval(replace_data, 500);             
+            mytimer = setInterval(replace_data, 500);             
         } else {
             clearInterval(mytimer);
         }
@@ -112,48 +124,36 @@ callbackAnime.args["renderer_source"] = renderer_source
 callbackAnime.args["plot"] = plot
 
 btn = Toggle(label="Play/Stop Animation", button_type="success",
-              active=False, callback=callbackAnime)
+              active=True, callback=callbackAnime)
 
 show(column(widgetbox(btn,slider),plot))
-
-#show(column(widgetbox(slider),plot))
-
 output_file("hourly.html")
 
-
-#from bokeh.io import curdoc
 #curdoc().clear()
 
-#Map of yearly growth in stations
+#Create station map with year slider
 
-file = '/Users/samanthafu/stations.csv' 
-df1 = pd.read_csv(file, skiprows=0)
-
-from bokeh.io import output_file, output_notebook, show
-from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
-    DataRange1d, PanTool, WheelZoomTool, BoxSelectTool, CustomJS, Slider, Toggle, Range1d, HoverTool, Text
-)
-from bokeh.models.mappers import ColorMapper, LinearColorMapper, CategoricalColorMapper
-from bokeh.palettes import Viridis
-from bokeh.layouts import column, row, widgetbox
-
+#Set center of map to central London
 map_options = GMapOptions(lat=51.504817, lng=-0.100186, map_type="roadmap", zoom=13)
 
 plot = GMapPlot(
     x_range=Range1d(), y_range=Range1d(), map_options=map_options, plot_width=1300, plot_height=700
 )
+
+#Format title
 plot.title.text = "Stations by Year Installed"
 plot.title.text_font = "amatic sc"
 plot.title.align = "center"
 plot.title.text_font_size="40px"
 
-plot.api_key = "AIzaSyDwcFI-fWLlqaLLm-vVomzJTzHej9QeW8Q"
+#Set api key
+plot.api_key = API_KEY
 
+#Create list of datasets to call
 sources = {}
 
 for x in range(2010, 2018):
-    newdf = df1[df1['year']==x]
+    newdf = df1[df1['year']<=x]
     sources['_{0}'.format(x)] = ColumnDataSource(
         data=dict(
             lat=newdf.lat.tolist(),
@@ -165,13 +165,14 @@ for x in range(2010, 2018):
         )
     )
 
+#Map circle colors to years
 color_mapper = CategoricalColorMapper(factors=[2017,2016,2015,2014,2013,2012,2011,2010], palette=['#08306b','#08519c','#2171b5','#4292c6','#6baed6','#9ecae1','#c6dbef','#deebf7'])
+
 circle = Circle(x="lon", y="lat", size="size", fill_color=dict(field='rate',transform=color_mapper), fill_alpha=0.8, line_color=None)
                       
 plot.add_glyph(sources['_2010'], circle)
 
 #Add tools
-
 hover= HoverTool(tooltips = [
     ("station","@name"),
     ("docks","@size"),
@@ -181,12 +182,10 @@ hover= HoverTool(tooltips = [
 plot.add_tools(PanTool(), WheelZoomTool(),hover)
 
 #Create dict of sources
-
 dictionary_of_sources = dict(zip([x for x in range(2010,2018)], ['_%s' % x for x in range(2010,2018)]))
 js_source_array = str(dictionary_of_sources).replace("'", "")
 
 #Add slider with callback to update data source
-
 renderer_source = sources['_2010']
 code = """
     var year = cb_obj.value,
@@ -200,7 +199,8 @@ slider = Slider(start=2010, end=2017, value=0, step=1, title="Year Installed",  
 
 callback.args["renderer_source"] = renderer_source
 
+#show(column(widgetbox(btn,slider),plot))
+
+output_notebook(resources=INLINE)
 show(column(widgetbox(slider),plot))
-
 output_file("stations_slider.html")
-
